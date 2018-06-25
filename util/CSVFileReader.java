88ec9ccb25bc7cb014afae195541ca8dd101b1ba
Ai-yoo <duzhentong123@foxmail.com>
2018-06-25 10:08:11 +0800

    读取csv文件工具类

--- util/CSVFileReader.java
+++ util/CSVFileReader.java
@@ -7,55 +7,40 @@ import java.util.ArrayList;
  * Created with IDEA
  *
  * @author duzhentong
- * @Date 2018/6/25
- * @Time 9:10
+ * @Date 2018/6/24
+ * @Time 19:05
  */
 public class CSVFileReader {
-    public static void formatFile() {
-        String filepath = "D:\\testfiles\\a.txt";
+    public static ArrayList<Node> readFile(){
         BufferedReader reader = null;
         ArrayList<Node> list = new ArrayList<Node>();
         File file = null;
         InputStreamReader isr = null;
-        String[] str = null;
-        FileOutputStream fos = null;
         try {
-            file = new File(filepath);
+            file = new File("E:\\IntelliJ IDEA\\libsvmtest\\src\\main\\resources\\training-inspur.csv");
             isr = new InputStreamReader(new FileInputStream(file), "UTF-8");
             reader = new BufferedReader(isr);
+            reader.readLine();
             String line = null;
-            line = reader.readLine();
-            str = line.split(".");
-            for (String s : str) {
-                System.out.println(s);
-                char ch = s.charAt(0);
-                if (ch == '0') {
-                    s.replaceFirst("^0", "\r\n");
-                }
+            while((line=reader.readLine())!=null){
+                int begin = line.indexOf(",");
+                int end = line.lastIndexOf(",");
+                int id = Integer.parseInt(line.substring(0, begin));
+                String comment = line.substring(begin + 1, end);
+                int classes = Integer.parseInt(line.substring(end + 1, line.length()));
+                Node node = new Node(id, comment, classes, null);
+                list.add(node);
             }
-            String result = str.toString();
-            byte[] bytes = result.getBytes();
-            fos = new FileOutputStream(filepath);
-            fos.write(bytes);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            try {
-                reader.close();
-                isr.close();
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+        for (Node n : list) {
+            System.out.println(n.toString());
+        }
+        return list;
     }
 
-    public static void main(String[] args) {
-        formatFile();
-    }
 
 }
